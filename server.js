19a4c4332fe35cca9dad9835adb4505e0f6f7367
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-26 21:33:47 -0500

    added a route to make sure all is working.

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT
 
 const app = express()
 
+// Middleware
+app.use(express.json())
+app.use(bodyParser.json())
+
+// Routes
+app.get('/', (req, res) => {
+     res.send('Home Page')
+})
+
 mongoose.connect(process.env.MONGO)
      .then(() => {
           app.listen(PORT, () => { console.log(`This server is running on port ${PORT}`) })
