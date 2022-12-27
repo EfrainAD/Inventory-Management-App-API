7ed1c57f8d2493f9f36075cbe60459c1cd623542
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-27 12:52:18 -0500

    Create a user to the DB

--- controllers/user-controllor.js
+++ controllers/user-controllor.js
@@ -1,10 +1,45 @@
-const registerUser = async (req, res) => {
-     if (!req.body.email) {
+const asyncHandler = require('express-async-handler')
+const User = require('../models/user-model')
+
+const registerUser = asyncHandler(async (req, res) => {
+     const { name, email, password } = req.body
+     const emailValidater = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+     
+     // Validation
+     if (!name, !email, !password) {
           res.status(400)
-          throw new Error('Must have an email')
+          throw new Error('Pllease fill in a required missing fields')
      }
-     res.send('Register User')
-}
+     if (!emailValidater.test(email.trim())) {
+          res.status(400)
+          throw new Error("Email not valid")
+     }
+     if (password.length < 8) {
+          res.status(400)
+          throw new Error('Password must be at least 8 characters long')
+     }
+     if (password.length >= 23) {
+          res.status(400)
+          throw new Error('Password must be shorter then 23 characters long')
+     }
+     // check is user already exist.
+     const existsUser = await User.findOne({email: email})
+     if (existsUser) {
+          res.status(400)
+          throw new Error('User already exists with this email')
+     }
+     // create user
+     const user = await User.create({name, email, password})
+     if (user) {
+          const {_id, name, email, photo, phone, bio} = user
+          res.status(201).json({
+               _id, name, email, photo, phone, bio
+          })
+     } else {
+          res.status(400)
+          throw new Error('Invalid user data')
+     }
+})
 
 module.exports = {
      registerUser,
--- models/user-model.js
+++ models/user-model.js
@@ -14,26 +14,26 @@ const userSchema = mongoose.Schema({
                /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
                "Email not valid"
           ],
-          password: {
-               type: String,
-               required: [true, 'Must have a password'],
-               minLength: [8, 'Password must be at least 8 characters long'],
-               maxLength: [23, 'Password must be shorter then 23 characters long']
-          },
-          photo: {
-               type: String,
-               required: [true, 'Must have a photo'],
-               default: 'https://i.ibb.co/4pDNDk1/avatar.png'
-          },
-          phone: {
-               type: String,
-               default: '+1'
-          },
-          bio: {
-               type: String,
-               maxLength: [250, 'Bio must be shorter then 250 characters long'],
-               default: 'bio'
-          }
+     },
+     password: {
+          type: String,
+          required: [true, 'Must have a password'],
+          minLength: [8, 'Password must be at least 8 characters long'],
+          maxLength: [23, 'Password must be shorter then 23 characters long']
+     },
+     photo: {
+          type: String,
+          required: [true, 'Must have a photo'],
+          default: 'https://i.ibb.co/4pDNDk1/avatar.png'
+     },
+     phone: {
+          type: String,
+          default: '+1'
+     },
+     bio: {
+          type: String,
+          maxLength: [250, 'Bio must be shorter then 250 characters long'],
+          default: 'bio'
      }
 },{
      timestamps: true
