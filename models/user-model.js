5337462dd1668402c1fa15cb8960d18fed3f4708
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-27 16:51:18 -0500

    refractored encryption into model, so you don't need to recall it everytime to change the password but it will do it for you.

--- models/user-model.js
+++ models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
      name: {
@@ -37,6 +38,17 @@ const userSchema = mongoose.Schema({
      }
 },{
      timestamps: true
+})
+
+// Encrypt Password
+userSchema.pre('save', async function(next) {
+     if (!this.isModified('password')) 
+          return next()
+
+     const salt = await bcrypt.genSalt(10)
+     const hashedPassword = await bcrypt.hash(this.password, salt)
+     this.password = hashedPassword
+     next()
 })
 
 const User = mongoose.model('User', userSchema)
