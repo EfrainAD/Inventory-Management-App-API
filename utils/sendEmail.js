22d0f9c8ff17bf24905e111ae81593a138918ab1
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-30 22:34:04 -0500

    Looked up documentation and saw I had made a mistake on the naming of the option, easy fix, wish I had just looked there first.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -17,7 +17,7 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
      const options = {
           from: sent_from,
           to: send_to,
-          reply: reply_to,
+          replyTo: reply_to,
           subject: subject,
           html: message,
      }
