6ad556b298244e43c22bba07c803f54b6a6c5b38
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-02-10 17:53:43 -0500

    Removed cors

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({
-     // origin: ['http://localhost:3000', frontPageUrl ],
-     credentials: true
-}));
+// app.use(cors({
+//      // origin: ['http://localhost:3000', frontPageUrl ],
+//      credentials: true
+// }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
